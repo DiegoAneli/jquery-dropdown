57d5d78bf13650a7048adad71d213f8e1a98e7cb
DiegoA <diegoaneli@gmail.com>
2018-11-29 21:37:50 +0100

    jquery-dropdown

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-$('.classe')
+$('.item').click(function() {
+  $('.dropdown_menu').removeClass('show');
+  // $(this).children('.dropdown_menu').slideDown();
+
+  var thisSingoloMenu = $(this).children('.dropdown_menu');
+  thisSingoloMenu.addClass('show');
+
+});
